238accd6282d91cfd416f2d83b6e4febb0a6214f
Harshitha1909195 <harshithadik@gmail.com>
2025-08-18 16:10:20 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,11 @@
 #pragma once
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+//int vitalsOk(float temperature, float pulseRate, float spo2);
+
+bool isTemperatureOk(float temperature);
+bool isPulseRateOk(float pulseRate);
+bool isSpo2Ok(float spo2);
+bool vitalsOk(float temperature, float pulseRate, float spo2);
+void alertVitalOutOfRange(const char* message);
+void blinkWarning();
+
